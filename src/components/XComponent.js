74cdd3b10548cca4312f3c7ece910c49bf88c073
zowiegong <zowiegong@gmail.com>
2019-03-09 14:16:09 +0800

    update

--- src/components/XComponent.js
+++ src/components/XComponent.js
@@ -1,4 +1,4 @@
-import { fmtJSXItems } from '../utils/helpers'
+import { fmtJSXConfig } from '../utils/helpers'
 
 const groupMap = {
   checkbox: ['ElCheckboxGroup', 'ElCheckbox'],
@@ -6,22 +6,19 @@ const groupMap = {
   radio: ['ElRadioGroup', 'ElRadio']
 }
 
-// 带 group 组件 render
 const groupRender = v => {
   // eslint-disable-next-line
   const h = v.$createElement
 
   return config => {
     const { type, component: { $parent, $children } } = config
-    if (!(type in groupMap)) throw Error(`Type error`)
-
     const [$parentName, $childrenName] = groupMap[type]
 
     return (
-      <$parentName {...$parent}>
+      <$parentName {...fmtJSXConfig($parent)}>
         {
           $children.map(i => (
-            <$childrenName {...i} >{ i.$text }</$childrenName>
+            <$childrenName {...fmtJSXConfig(i)} >{ i.$text }</$childrenName>
           ))
         }
       </$parentName>
@@ -35,13 +32,12 @@ export default function XComponent (v) {
   const h = v.$createElement
 
   return function (config) {
-    config = fmtJSXItems([config])[0]
     const { type, component } = config
 
     if (Object.prototype.toString.call(component) === '[object Function]') return component.call(v)
     if ('$parent' in component && '$children' in component) return groupRender(v)(config)
 
     const tagName = 'El' + type.charAt(0).toUpperCase() + type.slice(1)
-    return <tagName {...component}>{ component.$text }</tagName>
+    return <tagName {...fmtJSXConfig(component)}>{ component.$text }</tagName>
   }
 }
