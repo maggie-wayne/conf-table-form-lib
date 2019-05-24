3c9298412a36a2e84effcc1ee00667985ba99fb9
maggieyang1994 <maggieyang1994@gmail.com>
2019-05-24 11:36:22 +0800

    * add deploy to gh-pages

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: '/conf-table-form-lib/',
+  outputDir: 'page',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
